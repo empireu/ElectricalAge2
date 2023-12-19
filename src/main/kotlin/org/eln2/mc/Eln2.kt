81aca329117aa54763a5ba36f54bff54df741616
empireu <rodney.mckay.meredith@gmail.com>
2023-12-19 03:45:27 +0200

    ngmi

--- src/main/kotlin/org/eln2/mc/Eln2.kt
+++ src/main/kotlin/org/eln2/mc/Eln2.kt
@@ -6,7 +6,6 @@ import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.packs.resources.Resource
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.common.MinecraftForge
-import net.minecraftforge.data.event.GatherDataEvent
 import net.minecraftforge.eventbus.api.IEventBus
 import net.minecraftforge.fml.DistExecutor
 import net.minecraftforge.fml.ModLoadingContext
