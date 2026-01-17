72fa0ba5a6cc67228699aa8a8045c43e04a7968b
empireu <rodney.mckay.meredith@gmail.com>
2026-01-17 15:39:17 +0200

    Gets a launchable version working by adding an alias to the mixin field, and relocating the shadowed dependencies we could

--- src/main/java/org/eln2/mc/mixin/MixinLightEngine.java
+++ src/main/java/org/eln2/mc/mixin/MixinLightEngine.java
@@ -25,7 +25,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LightEngine.class)
 public abstract class MixinLightEngine {
     @Final
-    @Shadow
+    @Shadow(aliases = {"f_75836_"})
     protected LightChunkGetter chunkSource;
 
     @Unique
